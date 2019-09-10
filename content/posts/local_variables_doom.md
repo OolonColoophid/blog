32a03e8421175f96cd71e36b05446ff05ad771b6
Ian Hocking @ Winnetou <ihocking@gmail.com>
2019-09-10 10:24:29 +0100

    Test pre-push hook

--- content/posts/local_variables_doom.md
+++ content/posts/local_variables_doom.md
@@ -2,10 +2,11 @@
 title = "Permanently Set File-Local Variables in Doom Emacs"
 author = ["Dr Ian Hocking"]
 date = 2019-09-09
+lastmod = 2019-09-10T10:23:51+01:00
 draft = false
 +++
 
-From time to time, it is useful to set local variables within an `org`
+Occasionally, it is useful to set local variables within an `org`
 file. For instance, I have a file whose subsections I'd like to occasionally
 encrypt. This being the case, I need to turn off autosave (since this could save
 plain text). To do this for the buffer, I add this to the top:
--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,22 @@
 #!/usr/bin/env bash
 
-# Sync /public to webserver
+# Sync /public (containing finalised HTML and resources)
+# to webserver (e.g. ianhocking.com)
 
 # e - exit if command exits with error
 # u - treat unset variables as an error
 set -eu
 
-username=ianhocki
-server=ianhocking.com
+username="ianhocki"
+server="ianhocking.com"
 
 blogDir="/Users/ianuser/Dropbox/org/blog/programmer"
 
-# Check where this script is running from.
-
-if [ -z "$GIT_DIR" ] ; then
-
-    echo "Run deploy.sh (for ianhocking.com/blog) from ~/Dropbox/org/" >&2
-    exit 1
-
-fi
-
 # Remove previous build of site
-rm -r public || echo "No /public directory to delete"
+rm -r "$blogDir/public" || echo "No /public directory in blog directory $blogDir to delete"
 
 # Build
-hugo
+cd $blogDir && hugo || echo "Cannot cd to blog directory $blogDir"
 
 # Upload
-rsync -r --verbose --compress --human-readable --progress --recursive public/. ianhocki@ianhocking.com:public_html/blog/
+rsync -r --verbose --compress --human-readable --progress --recursive public/. $username@$server:public_html/blog/

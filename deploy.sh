c5ddcc408b2b4a1d5421437076e0090350dd6574
Ian Hocking <ihocking@gmail.com>
2021-05-06 14:17:13 +0100

    Update about page

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@
 # u - treat unset variables as an error
 set -eu
 
-dir="/Users/ianuser/Sync/org/blog/programmer/"
+dir="/Users/ianuser/Library/Mobile Documents/com~apple~CloudDocs/Documents/org/blog/programmer/"
 logFile="logs/hooks.log"
 
 echo "---deploy.sh now takes over---" >> "$dir$logFile"
